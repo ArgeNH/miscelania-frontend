4b8ae74517af7634f1fb2cff2e19c27f266b82b2
Arge Niño <arge.nino@uptc.edu.co>
2022-01-22 22:51:24 -0500

    product select page, config routes

--- src/routes/DashboardRoutes.js
+++ src/routes/DashboardRoutes.js
@@ -13,30 +13,32 @@ import { Others } from '../components/product/category/Others';
 import { Pharmacy } from '../components/product/category/Pharmacy';
 import { Stationery } from '../components/product/category/Stationery';
 import { ProductScreen } from '../components/product/ProductScreen';
+import { ProductSelected } from '../components/product/ProductSelected';
 import { PromotionScreen } from '../components/promotion/PromotionScreen';
 
 export const DashboardRoutes = () => {
-    return (
-        <>
-            <Nabvar />
-            <div className='container mx-auto px-4'>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='productos' element={<ProductScreen />} />
-                    <Route path='promociones' element={<PromotionScreen />} />
-                    <Route path='compra' element={<ShoppingScreen />} />
-                    <Route path='papeleria' element={<Stationery />} />
-                    <Route path='farmacia' element={<Pharmacy />} />
-                    <Route path='aseo' element={<Cleanliness />} />
-                    <Route path='hogar' element={<Menage />} />
-                    <Route path='ferreteria' element={<IronStore />} />
-                    <Route path='otros' element={<Others />} />
+   return (
+      <>
+         <Nabvar />
+         <div className='container mx-auto px-4'>
+            <Routes>
+               <Route path='/' element={<Home />} />
+               <Route path='productos' element={<ProductScreen />} />
+               <Route path='promociones' element={<PromotionScreen />} />
+               <Route path='compra' element={<ShoppingScreen />} />
+               <Route path='papeleria' element={<Stationery />} />
+               <Route path='farmacia' element={<Pharmacy />} />
+               <Route path='aseo' element={<Cleanliness />} />
+               <Route path='hogar' element={<Menage />} />
+               <Route path='ferreteria' element={<IronStore />} />
+               <Route path='otros' element={<Others />} />
+               <Route path='productos/:code/:name' element={<ProductSelected />} />
 
-                    <Route path='404' element={<Error404 />} />
-                    <Route path='*' element={<Navigate to='/404' />} />
-                </Routes>
-            </div>
-            <Footer />
-        </>
-    )
+               <Route path='404' element={<Error404 />} />
+               <Route path='*' element={<Navigate to='/404' />} />
+            </Routes>
+         </div>
+         <Footer />
+      </>
+   )
 }

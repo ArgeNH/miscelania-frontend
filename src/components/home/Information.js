fb425a327b12a4a4e863d831f81e2ad60258077a
Arge Niño <arge.nino@uptc.edu.co>
2022-03-22 23:41:22 -0500

    change in Navbar, and add logo

--- src/components/home/Information.js
+++ src/components/home/Information.js
@@ -1,29 +1,16 @@
 import React from 'react';
+import { getImage } from '../../helpers/getImage';
 
 export const Information = () => {
    return (
       <section className="text-gray-600 body-font bg-slate-200 mt-2 rounded-lg border-double border-4 border-slate-300">
          <div className="container px-5 pt-20 mx-auto flex flex-col">
             <div className="lg:w-4/6 mx-auto">
-               {/* <div className="rounded-lg h-64 overflow-hidden">
-                  <img alt="content" className="object-center h-full w-full xl:object-cover" src="/assets/logo.png" />
-               </div> */}
                <div className="flex flex-col sm:flex-row mt-2">
                   <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                     <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 24 24">
-                           <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                           <circle cx="12" cy="7" r="4"></circle>
-                        </svg>
-                     </div>
-                     <div className="flex flex-col items-center text-center justify-center">
-                        <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">Nombre Propietario</h2>
-                        <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                        <p className="text-base">
-                           Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh
-                           hot chicken gentrify portland.
-                        </p>
-                     </div>
+                     
+                  <img src={getImage('./Logo.png')} alt="Logo" className='mx-auto w-auto h-auto object-center' />
+
                   </div>
                   <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
                      <p className="leading-relaxed text-lg mb-4">
